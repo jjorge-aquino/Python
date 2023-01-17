6d0e39bb5835e01ba08abbd0dcfee9630388042c
jjorge-aquino <81631243+jjorge-aquino@users.noreply.github.com>
2023-01-17 13:34:14 -0300

    asdad

---
+++ Jasper/GeraPdfV2.py
@@ -0,0 +1,30 @@
+import os
+from platform import python_version
+from pyreportjasper import PyReportJasper
+
+def advanced_example_using_database():
+   REPORTS_DIR = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'reports')
+   input_file = os.path.join(REPORTS_DIR, 'hello_world.jrxml')
+   output_file = os.path.join(REPORTS_DIR, 'hello_world')
+   # Define a conexão com InterSystems Cache
+   connection_string = 'jdbc:Cache://localhost:57772/namespace'
+   '''conn = {
+     'driver': 'postgres',
+     'username': 'DB_USERNAME',
+     'password': 'DB_PASSWORD',
+     'host': 'DB_HOST',
+     'database': 'DB_DATABASE',
+     'schema': 'DB_SCHEMA',
+     'port': '5432'
+     'jdbc_dir': '<path>/postgres.jar'
+   }'''
+   pyreportjasper = PyReportJasper()
+   pyreportjasper.config(
+     input_file,
+     output_file,
+     connection_string=connection_string,
+     output_formats=["pdf", "rtf"],
+     parameters={'python_version': python_version()},
+     locale='en_US'
+   )
+   pyreportjasper.process_report()
